2cf34d6e374c14d04591763460b69929d009259a
Stephen Sinclair <smsinclair@gmail.com>
2019-11-06 14:23:44 -0500

    finished stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,13 +3,22 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    if len(prices) < 2:
+        return "prices must contain two or more integers"
+    max_profit = prices[1] - prices[0]
+    for i in range(0, len(prices)):
+        for j in range(i+1, len(prices)):
+            profit = prices[j] - prices[i]
+            if profit > max_profit:
+                max_profit = profit
+    return max_profit
+print(find_max_profit([10, 7, 5, 8, 11, 9]))
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
