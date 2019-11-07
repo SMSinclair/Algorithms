113b6829354f766f4c4acebf95d6558c5f314008
Stephen Sinclair <smsinclair@gmail.com>
2019-11-07 17:09:22 -0500

    finished rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,23 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+
+    def rps_recursive(n):
+        choices = [['rock'], ['paper'], ['scissors']]
+        if n == 0:
+            return [[]]
+        if n == 1:
+            return choices
+
+        result = []
+        for element in rps_recursive(n-1):
+            for choice in choices:
+                result.append(element + choice)
+        return result
+
+    return rps_recursive(n)
+
+
 
 
 if __name__ == "__main__":
@@ -12,3 +28,4 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+
