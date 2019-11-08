8f3ea8571214b69b7ac30d39c49f6f0a58e08776
Stephen Sinclair <smsinclair@gmail.com>
2019-11-08 16:52:42 -0500

    knapsack sm pass

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -6,8 +6,25 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  pass
-  
+    ratio = [0] * len(items)
+    total_size = 0
+    sorted_items = sorted(items, key = lambda x: x.value/x.size, reverse=True)
+    total_value = 0
+    chosen = []
+
+    for i in range(0, len(sorted_items)):
+        ratio[i] = sorted_items[i].value/sorted_items[i].size
+        print(sorted_items[i])
+
+    for item in sorted_items:
+        if total_size + item.size <= capacity:
+            total_value += item.value
+            total_size += item.size
+            chosen.append(item.index)
+
+    result = {'Chosen': sorted(chosen), 'Value': total_value, }
+    return result
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
